f844286c002356a684dad0cfc1e6dd2be1712ca9
Subodh Adhikari <subodhadhikari2023@outlook.com>
2026-07-28 13:24:31 +0530

    Fix formatting in prisma.module.ts and prisma.service.ts

--- api/src/prisma/prisma.service.ts
+++ api/src/prisma/prisma.service.ts
@@ -2,24 +2,22 @@ import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '../../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
-
-
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit,OnModuleDestroy {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
+    });
+  }
 
-    constructor() {
-        super({
-           adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
+  async onModuleInit() {
+    await this.$connect();
+  }
 
-        });
-    }
-
-
-    async onModuleInit() {
-        await this.$connect();
-    }
-    
-    async onModuleDestroy() {
-        await this.$disconnect();
-    }
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
 }
